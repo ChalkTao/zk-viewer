a779512dc6ae27850208256c0bd419b4d1dd5d0a
caotao <caotao@xiaomi.com>
2016-12-02 21:44:00 +0800

    add angular

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var data = chrome.extension.getBackgroundPage().articleData;
-	if(data.error){
-		$("#message").text(data.error);
-		$("#content").hide();
-	}else{
-		$("#message").hide();
-		$("#content-title").text(data.title);
-		$("#content-author").text(data.author);
-		$("#content-date").text(data.postDate);
-		$("#content-first-access").text(data.firstAccess);
-	}
+var config = {
+    "env": ["staging", "lugu", "c3", "aws_sgp"],
+    "default_path": {
+        "SDK": "/services/com.xiaomi.miui.ad.thrift.service.MiuiAdQueryService/AppConfig",
+        "MAX": "/services/com.xiaomi.miui.ad.exchange.thrift.bid.service.MiuiAdExchangeBidService/AppConfig",
+        "Query": "/services/com.xiaomi.miui.ad.thrift.service.MiuiAdQueryService/AppConfig",
+        "Retrieve": "/services/com.xiaomi.miui.ad.core.common.model.adretrieve.MiuiAdRetrieveService/AppConfig"
+    }
+};
+
+angular.module('zkView', []).controller('ZKViewController', function($scope, $http) {
+    $scope.default_path = Object.keys(config.default_path);
+    $scope.path = "";
+    $scope.path_tag = "常用环境";
+    $scope.key = "";
+    $scope.env_list = config.env;
+    $scope.cur_env = 0;
+
+    $scope.select_path = function(item) {
+        $scope.path = config.default_path[item];
+        $scope.path_tag = item;
+    };
+
+    $scope.search = function() {
+        get_zk_config();
+    };
+
+    $scope.change_env = function(index) {
+        $scope.cur_env = index;
+        get_zk_config();
+    };
+    
+    function get_zk_config() {
+        $http.jsonp('http://www.baidu.com').success(function(data) {
+            alert(data);
+        });
+    }
 });
